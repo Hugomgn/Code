61af4e67dd2073dfef4d39dd163e385e498a7fea
Hugomgn <112679326+Hugomgn@users.noreply.github.com>
2022-12-08 21:29:34 +0100

    Delete Notions de Base directory

--- Notions de Base/base.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream> // Directive de préprocesseur : Bibliothèques input/output standard.
-
-int main() // Fonction principale
-{
-  std::cout << "Hello World !" << std::endl; //afficher Hello world, puis revenir a la ligne
-
-  return 0; // retourner 0, fin du programme.
-}
